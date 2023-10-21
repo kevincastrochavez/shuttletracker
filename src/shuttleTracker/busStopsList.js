cb90c234216b8d3ed302649a8a12fda02003d8a1
Kevin Castro <cas20057@byui.edu>
2023-10-21 03:38:54 -0600

    created object wit exact longitud and latitude for all shuttle stops

---
+++ src/shuttleTracker/busStopsList.js
@@ -0,0 +1,64 @@
+const busStopsObj = [
+  {
+    Walmart: {
+      location: { lat: 43.85632113354398, lng: -111.77452942286942 },
+      stopover: true,
+    },
+  },
+  {
+    'East MC Circle': {
+      location: { lat: 43.81770149069237, lng: -111.78094808430095 },
+      stopover: true,
+    },
+  },
+  {
+    'Aspen Village': {
+      location: { lat: 43.81395587316384, lng: -111.77687980167565 },
+      stopover: true,
+    },
+  },
+  {
+    'Center Square': {
+      location: { lat: 43.81200576964376, lng: -111.78675856722327 },
+      stopover: true,
+    },
+  },
+  {
+    'The Gates': {
+      location: { lat: 43.813565416627604, lng: -111.79406947681908 },
+      stopover: true,
+    },
+  },
+  {
+    'Camden Apartments': {
+      location: { lat: 43.81554208566595, lng: -111.79114066017077 },
+      stopover: true,
+    },
+  },
+  {
+    'Colonial House': {
+      location: { lat: 43.8176973463876, lng: -111.78827487264415 },
+      stopover: true,
+    },
+  },
+  {
+    'BYU-I Hart': {
+      location: { lat: 43.81918403818366, lng: -111.78666630951572 },
+      stopover: true,
+    },
+  },
+  {
+    'BYU-I Parking Lot': {
+      location: { lat: 43.822078255337544, lng: -111.78321563274208 },
+      stopover: true,
+    },
+  },
+  {
+    'Rexburg Floral': {
+      location: { lat: 43.830036152465446, lng: -111.78375904014248 },
+      stopover: true,
+    },
+  },
+];
+
+export default busStopsObj;

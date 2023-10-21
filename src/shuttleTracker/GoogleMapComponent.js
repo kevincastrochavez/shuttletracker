97251057b5996a9262499078e22e1d20cbb8c9cc
Kevin Castro <cas20057@byui.edu>
2023-10-21 03:13:48 -0600

    got DirectionServices to work

--- src/shuttleTracker/GoogleMapComponent.js
+++ src/shuttleTracker/GoogleMapComponent.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+} from '@react-google-maps/api';
 import { Skeleton } from '@mantine/core';
 import { Box, Flex } from '@chakra-ui/react';
 
@@ -7,17 +12,78 @@ import { useBusInfo } from './ShuttleTrackerProvider';
 import car from './car.png';
 
 function GoogleMapComponent() {
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  });
-
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const middleOfRexburgCoords = {
     lat: 43.82402030515836,
     lng: -111.78097057734374,
   };
+  const walmartCoords = { lat: 43.854752, lng: -111.777962 };
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  const [map, setMap] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
   const center = middleOfRexburgCoords;
   const { busLocation } = useBusInfo();
+
+  const onLoad = React.useCallback(async function callback(map) {
+    setMap(map);
+
+    // Get directions
+    const google = window.google;
+    const directionsService = new google.maps.DirectionsService();
+
+    const results = await directionsService.route({
+      origin: walmartCoords,
+      destination: walmartCoords,
+      waypoints: [
+        {
+          location: { lat: 43.81778, lng: -111.78084 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.814201, lng: -111.778122 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.81180247920654, lng: -111.78677373375258 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.813570621409866, lng: -111.79428811938575 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.81561946956219, lng: -111.79476980506578 },
+          stopover: false,
+        },
+        {
+          location: { lat: 43.8155133, lng: -111.7903156 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.817596, lng: -111.788459 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.8195804, lng: -111.7868141 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.8221074545991, lng: -111.78322995479034 },
+          stopover: true,
+        },
+        {
+          location: { lat: 43.8297894, lng: -111.7839334 },
+          stopover: true,
+        },
+      ],
+      travelMode: 'DRIVING',
+    });
+
+    setDirectionsResponse(results);
+  }, []);
 
   if (!isLoaded) return <Skeleton height={400} radius='md' />;
 
@@ -32,7 +98,7 @@ function GoogleMapComponent() {
         <GoogleMap
           center={center}
           zoom={13}
-          onLoad={(map) => setMap(map)}
+          onLoad={onLoad}
           mapContainerStyle={{ width: '100%', height: '100%' }}
           options={{
             zoomControl: false,
@@ -41,6 +107,9 @@ function GoogleMapComponent() {
           }}
         >
           <Marker position={busLocation} icon={car} map={map} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
         </GoogleMap>
       </Box>
     </Flex>

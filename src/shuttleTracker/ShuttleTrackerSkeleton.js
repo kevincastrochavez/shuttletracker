18e963cb53e5574a8f9db803a6db3be1a2e00292
Kevin Castro <cas20057@byui.edu>
2023-10-20 22:25:46 -0600

    finished skeleton

--- src/shuttleTracker/ShuttleTrackerSkeleton.js
+++ src/shuttleTracker/ShuttleTrackerSkeleton.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Skeleton } from '@mantine/core';
 
+import classes from './ShuttleTrackerSkeleton.module.css';
+
 function ShuttleTrackerSkeleton() {
   return (
     <>
       <Skeleton height={16} mt={6} radius='xl' width={'60%'} />
       <Skeleton height={14} mt={6} radius='xl' width={'75%'} />
-      <Skeleton height={40} mt={6} radius='sm' />
+      <Skeleton height={40} mt={6} radius='md' />
+      <Skeleton height={16} mt={30} radius='xl' width={'75%'} />
+      <Skeleton height={400} mt={60} radius='md' />
+      <div className={classes.avatarContainer}>
+        <Skeleton height={100} circle />
+        <div>
+          <Skeleton height={30} mt={6} radius='xl' width={'75%'} />
+          <Skeleton height={16} mt={6} radius='xl' width={'75%'} />
+        </div>
+      </div>
     </>
   );
 }

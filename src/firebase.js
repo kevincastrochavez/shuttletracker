8a510da5f56dc0d69fdabf5fc4222bdc321480a2
Kevin Castro <cas20057@byui.edu>
2023-10-20 19:49:54 -0600

    installed firebase and setup project keys, lazy loaded the main component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAqmIVk0s4ce5LAEBz7STkqdzgSCP3G9iQ',
+  authDomain: 'shuttle-tracker-3997d.firebaseapp.com',
+  databaseURL: 'https://shuttle-tracker-3997d-default-rtdb.firebaseio.com',
+  projectId: 'shuttle-tracker-3997d',
+  storageBucket: 'shuttle-tracker-3997d.appspot.com',
+  messagingSenderId: '1005418899561',
+  appId: '1:1005418899561:web:6269b4774ba12176aa5af8',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ShuttleTracker from './shuttleTracker/ShuttleTracker';
+
+const ShuttleTracker = lazy(() => import('./ShuttleTracker/ShuttleTracker'));
 
 const router = createBrowserRouter([
   {

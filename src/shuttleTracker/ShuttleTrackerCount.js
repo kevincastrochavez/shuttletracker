19474d84a71b249660e3742f070c827a1dbbe50e
cindycas <cha19032@byui.edu>
2023-10-21 13:46:12 -0600

    navbar stuff css

--- src/shuttleTracker/ShuttleTrackerCount.js
+++ src/shuttleTracker/ShuttleTrackerCount.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import classes from './ShuttleTrackerCount.module.css';
-import bus from './images/bus.svg';
-import { useBusInfo, useCarType } from './ShuttleTrackerProvider';
+import classes from "./ShuttleTrackerCount.module.css";
+import bus from "./images/bus.svg";
+import { useBusInfo, useCarType } from "./ShuttleTrackerProvider";
 
 /**
  * Component to display how many available seats there are on the van or bus
@@ -17,10 +17,10 @@ function ShuttleTrackerCount() {
 
   return (
     <div className={classes.countContainer}>
-      <h2>{busType === 'bus' ? 'BUS' : 'VAN'}</h2>
-      <img src={bus} alt='' />
+      <h2>{busType === "bus" ? "BUS" : "VAN"}</h2>
+      <img src={bus} alt="" />
       <p>
-        There are currently <b>{seatsAvailable}</b> out of <b>{totalSeats}</b>{' '}
+        There are currently <b>{seatsAvailable}</b> out of <b>{totalSeats}</b>{" "}
         seats available.
       </p>
       <div></div>

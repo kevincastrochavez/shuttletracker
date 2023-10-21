32679282cd6c76290878cffb671e3105463bbd41
Kevin Castro <cas20057@byui.edu>
2023-10-20 19:10:31 -0600

    installed react router dom and set up basic router and route

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ShuttleTracker from './shuttleTracker/ShuttleTracker';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ShuttleTracker />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

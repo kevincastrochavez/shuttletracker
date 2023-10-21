e17409f3cc1db07abf62b6a99a2990c6d7e79bac
cindycas <cha19032@byui.edu>
2023-10-21 10:43:43 -0600

    Navbar
    logo and navbar not responsive

--- src/shuttleTracker/ShuttleTrackerContent.js
+++ src/shuttleTracker/ShuttleTrackerContent.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Container, Space, Title } from '@mantine/core';
+import React from "react";
+import { Container, Space, Title } from "@mantine/core";
 
-import { useLocation } from './ShuttleTrackerProvider';
-import ShuttleTrackerSkeleton from './ShuttleTrackerSkeleton';
-import MinutesAway from './MinutesAway';
-import PreferredStop from './PreferredStop';
-import GoogleMapComponent from './GoogleMapComponent';
-import ShuttleTrackerFooter from './ShuttleTrackerFooter';
-import ShuttleTrackerDriver from './ShuttleTrackerDriver';
-import ShuttleTrackerCount from './ShuttleTrackerCount';
-import ContactForm from './ContactForm';
+import { useLocation } from "./ShuttleTrackerProvider";
+import ShuttleTrackerSkeleton from "./ShuttleTrackerSkeleton";
+import MinutesAway from "./MinutesAway";
+import PreferredStop from "./PreferredStop";
+import GoogleMapComponent from "./GoogleMapComponent";
+import ShuttleTrackerNavBar from "./ShuttleTrackerNavbar";
+import ShuttleTrackerFooter from "./ShuttleTrackerFooter";
+import ShuttleTrackerDriver from "./ShuttleTrackerDriver";
+import ShuttleTrackerCount from "./ShuttleTrackerCount";
+import ContactForm from "./ContactForm";
 
 /**
  * The main Shuttle Tracker content: stops list, minutes away & map
@@ -22,9 +23,10 @@ export default function ShuttleTrackerContent() {
   return (
     <>
       <Container fluid px={24} py={24}>
+        <ShuttleTrackerNavBar />
         <Title order={1}>Shuttle Live Tracking</Title>
-        <Space h='lg' />
-        <Space h='lg' />
+        <Space h="lg" />
+        <Space h="lg" />
         <>
           {locationLoading ? (
             <ShuttleTrackerSkeleton />
---
+++ src/shuttleTracker/ShuttleTrackerNavbar.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "./images/logo.png";
+
+import classes from "./ShuttleTrackerNavbar.module.css";
+
+function Footer() {
+  const [currentYear, setCurrentYear] = useState(0);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const date = new Date().getFullYear();
+    setCurrentYear(date);
+  });
+
+  return (
+    <div className={classes.navBarContainer}>
+      <div className={classes.navBarInfoBox}>
+        <div className={classes.navBarInfoBoxSingle}>
+          <Link to="/">
+            <img className={classes.navBarLogo} src={logo} />
+          </Link>
+        </div>
+
+        <div className={classes.navBarInfoBoxSingleLinks}>
+          <Link
+            to={"/"}
+            className={classes.navBarInfoBoxSingleLink}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            YOUR STOP
+          </Link>
+          <Link
+            to={"/"}
+            className={classes.navBarInfoBoxSingleLink}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            MAP
+          </Link>
+          <Link
+            to={"/"}
+            className={classes.navBarInfoBoxSingleLink}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            DRIVER
+          </Link>
+          <Link
+            to={"/"}
+            className={classes.navBarInfoBoxSingleLink}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            VEHICLE & SEATS
+          </Link>
+          <Link
+            to={"/"}
+            className={classes.navBarInfoBoxSingleLink}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            CONTACT US
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;

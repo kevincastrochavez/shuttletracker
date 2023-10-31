e3b7a2819f8665d5c22bc7628661683f1ecd46a4
Kevin Castro <cas20057@byui.edu>
2023-10-30 23:39:36 -0600

    hid navlinks dynamically

--- src/shuttleTracker/ShuttleTrackerNavbar.js
+++ src/shuttleTracker/ShuttleTrackerNavbar.js
@@ -1,15 +1,22 @@
-import React, { useEffect, useState, useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import logo from "./images/logo.png";
+import React, { useEffect, useState, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from './images/logo.png';
 
-import classes from "./ShuttleTrackerNavbar.module.css";
+import classes from './ShuttleTrackerNavbar.module.css';
+import { useLocation, useNotifications } from './ShuttleTrackerProvider';
 
 function Footer() {
+  const {
+    notifications: { brokenDown },
+  } = useNotifications();
+  const {
+    location: { isDriving },
+  } = useLocation();
   const scrollPositionRef = useRef(0);
   const scrollToPosition = () => {
     window.scrollTo({
       top: scrollPositionRef.current,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
@@ -18,60 +25,68 @@ function Footer() {
       <div className={classes.navBarInfoBox}>
         <div className={classes.navBarInfoBoxSingle}>
           <Link
-            to="/"
+            to='/'
             className={classes.navBarLogo}
             onClick={() => {
               scrollPositionRef.current = 0; // Set the desired scroll position in pixels
               scrollToPosition();
             }}
           >
-            <img src={logo} alt="shuttle tracker logo" />
+            <img src={logo} alt='shuttle tracker logo' />
           </Link>
         </div>
 
-        <div className={classes.navBarInfoBoxSingleLinks}>
+        <div
+          className={`${classes.navBarInfoBoxSingleLinks} ${
+            (!isDriving || brokenDown) && classes.navBarInfoBoxSingleContact
+          }`}
+        >
+          {isDriving && !brokenDown && (
+            <>
+              <Link
+                to={'/'}
+                className={classes.navBarInfoBoxSingleLink}
+                onClick={() => {
+                  scrollPositionRef.current = 0; // Set the desired scroll position in pixels
+                  scrollToPosition();
+                }}
+              >
+                YOUR STOP
+              </Link>
+              <Link
+                to={'/'}
+                className={classes.navBarInfoBoxSingleLink}
+                onClick={() => {
+                  scrollPositionRef.current = 280; // Set the desired scroll position in pixels
+                  scrollToPosition();
+                }}
+              >
+                MAP
+              </Link>
+              <Link
+                to={'/'}
+                className={classes.navBarInfoBoxSingleLink}
+                onClick={() => {
+                  scrollPositionRef.current = 800; // Set the desired scroll position in pixels
+                  scrollToPosition();
+                }}
+              >
+                VEHICLE & SEATS
+              </Link>
+              <Link
+                to={'/'}
+                className={classes.navBarInfoBoxSingleLink}
+                onClick={() => {
+                  scrollPositionRef.current = 1150; // Set the desired scroll position in pixels
+                  scrollToPosition();
+                }}
+              >
+                DRIVER
+              </Link>
+            </>
+          )}
           <Link
-            to={"/"}
-            className={classes.navBarInfoBoxSingleLink}
-            onClick={() => {
-              scrollPositionRef.current = 0; // Set the desired scroll position in pixels
-              scrollToPosition();
-            }}
-          >
-            YOUR STOP
-          </Link>
-          <Link
-            to={"/"}
-            className={classes.navBarInfoBoxSingleLink}
-            onClick={() => {
-              scrollPositionRef.current = 280; // Set the desired scroll position in pixels
-              scrollToPosition();
-            }}
-          >
-            MAP
-          </Link>
-          <Link
-            to={"/"}
-            className={classes.navBarInfoBoxSingleLink}
-            onClick={() => {
-              scrollPositionRef.current = 800; // Set the desired scroll position in pixels
-              scrollToPosition();
-            }}
-          >
-            VEHICLE & SEATS
-          </Link>
-          <Link
-            to={"/"}
-            className={classes.navBarInfoBoxSingleLink}
-            onClick={() => {
-              scrollPositionRef.current = 1150; // Set the desired scroll position in pixels
-              scrollToPosition();
-            }}
-          >
-            DRIVER
-          </Link>
-          <Link
-            to={"./"}
+            to={'./'}
             className={classes.navBarInfoBoxSingleLink}
             onClick={() => {
               scrollPositionRef.current = 1550; // Set the desired scroll position in pixels

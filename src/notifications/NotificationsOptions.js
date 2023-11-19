247a5c0c89bdbd4cb37a3605bfb050d4200af38d
Kevin Castro <cas20057@byui.edu>
2023-11-18 17:11:37 -0700

    included the four notification switches

--- src/notifications/NotificationsOptions.js
+++ src/notifications/NotificationsOptions.js
@@ -5,9 +5,24 @@ import NotificationsSwitch from './NotificationsSwitch';
 
 function NotificationsOptions() {
   const [vehicleBrokenChecked, setVehicleBrokenChecked] = useState(false);
+  const [heavyTrafficChecked, setHeavyTrafficChecked] = useState(false);
+  const [detourChecked, setDetourChecked] = useState(false);
+  const [reducedHoursChecked, setReducedHoursChecked] = useState(false);
 
   const onVehicleBrokenChange = (e) => {
     setVehicleBrokenChecked(e.currentTarget.checked);
+  };
+
+  const onHeavyTrafficChange = (e) => {
+    setHeavyTrafficChecked(e.currentTarget.checked);
+  };
+
+  const onDetourChange = (e) => {
+    setDetourChecked(e.currentTarget.checked);
+  };
+
+  const onReducedHoursChange = (e) => {
+    setReducedHoursChecked(e.currentTarget.checked);
   };
 
   return (
@@ -16,6 +31,21 @@ function NotificationsOptions() {
         checked={vehicleBrokenChecked}
         onChange={onVehicleBrokenChange}
         label='Vehicle Broke Down'
+      />
+      <NotificationsSwitch
+        checked={heavyTrafficChecked}
+        onChange={onHeavyTrafficChange}
+        label='Heavy Traffic'
+      />
+      <NotificationsSwitch
+        checked={detourChecked}
+        onChange={onDetourChange}
+        label='Detour'
+      />
+      <NotificationsSwitch
+        checked={reducedHoursChecked}
+        onChange={onReducedHoursChange}
+        label='Reduced Service Hours'
       />
     </>
   );

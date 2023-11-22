505ee58deeecc79a594ce269a1fc9d2eb772026a
Kevin Castro <cas20057@byui.edu>
2023-11-21 20:21:24 -0700

    saved the state of the switch when changing, and wrote data to the database

--- src/notifications/NotificationsOptions.js
+++ src/notifications/NotificationsOptions.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { ref, set } from 'firebase/database';
+
+import { db } from '../firebase';
+import NotificationsSwitch from './NotificationsSwitch';
 import { useNotifications } from './NotificationsProvider';
 
-import NotificationsSwitch from './NotificationsSwitch';
-
+/**
+ * Returns the notification options to switch from the db
+ * @returns {JSX.Element}
+ */
 function NotificationsOptions() {
   const {
     vehicleBrokenChecked,
@@ -12,42 +18,93 @@ function NotificationsOptions() {
     heavyTrafficChecked,
   } = useNotifications();
 
-  // const onVehicleBrokenChange = (e) => {
-  //   setVehicleBrokenChecked(e.currentTarget.checked);
-  // };
+  /**
+   * Saves the state of the option notification to the database
+   * @param {Boolean} vehicleBrokenChecked true if bus is broken
+   * @param {Boolean} deviationChecked true if bus took a deviation
+   * @param {Boolean} reducedHoursChecked true if it's a holiday or reduced hours
+   * @param {Boolean} heavyTrafficChecked true if there is heavy traffic
+   * @returns {JSX.Element}
+   */
+  function saveNotificationsToDatabase(
+    vehicleBrokenChecked,
+    deviationChecked,
+    reducedHoursChecked,
+    heavyTrafficChecked
+  ) {
+    const notificationsRef = ref(db, 'notifications/');
+    const data = {
+      brokenDown: vehicleBrokenChecked,
+      deviation: deviationChecked,
+      reduce: reducedHoursChecked,
+      traffic: heavyTrafficChecked,
+    };
 
-  // const onHeavyTrafficChange = (e) => {
-  //   setHeavyTrafficChecked(e.currentTarget.checked);
-  // };
+    set(notificationsRef, data)
+      .then(() => {
+        console.log('Data saved successfully!');
+      })
+      .catch((error) => {
+        console.log('The write failed...');
+      });
+  }
 
-  // const onDetourChange = (e) => {
-  //   setDetourChecked(e.currentTarget.checked);
-  // };
+  const onVehicleBrokenChange = () => {
+    saveNotificationsToDatabase(
+      !vehicleBrokenChecked,
+      deviationChecked,
+      reducedHoursChecked,
+      heavyTrafficChecked
+    );
+  };
 
-  // const onReducedHoursChange = (e) => {
-  //   setReducedHoursChecked(e.currentTarget.checked);
-  // };
+  const onHeavyTrafficChange = (e) => {
+    saveNotificationsToDatabase(
+      vehicleBrokenChecked,
+      deviationChecked,
+      reducedHoursChecked,
+      !heavyTrafficChecked
+    );
+  };
+
+  const onDetourChange = (e) => {
+    saveNotificationsToDatabase(
+      vehicleBrokenChecked,
+      !deviationChecked,
+      reducedHoursChecked,
+      heavyTrafficChecked
+    );
+  };
+
+  const onReducedHoursChange = (e) => {
+    saveNotificationsToDatabase(
+      vehicleBrokenChecked,
+      deviationChecked,
+      !reducedHoursChecked,
+      heavyTrafficChecked
+    );
+  };
 
   return (
     <>
       <NotificationsSwitch
         checked={vehicleBrokenChecked}
-        // onChange={onVehicleBrokenChange}
+        onChange={onVehicleBrokenChange}
         label='Vehicle Broke Down'
       />
       <NotificationsSwitch
         checked={heavyTrafficChecked}
-        // onChange={onHeavyTrafficChange}
+        onChange={onHeavyTrafficChange}
         label='Heavy Traffic'
       />
       <NotificationsSwitch
         checked={deviationChecked}
-        // onChange={onDetourChange}
+        onChange={onDetourChange}
         label='Detour'
       />
       <NotificationsSwitch
         checked={reducedHoursChecked}
-        // onChange={onReducedHoursChange}
+        onChange={onReducedHoursChange}
         label='Reduced Service Hours'
       />
     </>
